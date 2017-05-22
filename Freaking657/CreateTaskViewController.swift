7e3e7263d9dc5ed0642d3be53c842c0d353f2f52
Tom Munhoven <tom@Eligarf.fritz.box>
2017-05-22 14:42:21 +0200

    Now fixed Main VC and Add Task VC

--- Freaking657/CreateTaskViewController.swift
+++ Freaking657/CreateTaskViewController.swift
@@ -9,21 +9,33 @@
 import UIKit
 
 class CreateTaskViewController: UIViewController {
-
+    
     @IBOutlet weak var taskNameTextField: UITextField!
     
     @IBOutlet weak var importantSwitch: UISwitch!
     
+    var previousVC = TasksViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
-
-    
-    
+        
+        // Create a task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous VC
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+        
     }
     
 }
--- Freaking657/TasksViewController.swift
+++ Freaking657/TasksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TasksViewController.swift
 //  Freaking657
 //
 //  Created by Tom Munhoven on 21/05/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -70,6 +70,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBAction func plusTapped(_ sender: Any) {
         
         performSegue(withIdentifier: "addSegue", sender: nil)
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let nextVC = segue.destination as! CreateTaskViewController
+        nextVC.previousVC = self
         
     }
     
